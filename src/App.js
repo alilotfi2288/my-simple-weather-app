d4634cdb09b2ca1857c553c603e741f2f3cc8865
alilotfi.dev <lotfi26.ali@gmail.com>
2020-07-18 09:45:12 +0430

    navbar is ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+class App extends Component {
+  state = {
+    unit: '',
+    city: ''
+  }
+  changeUnit = (unit) => {
+    this.setState({
+      unit
+    })
+  }
+  setCity = (city) => {
+    this.setState({
+      city
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+      <NavBar changeUnit={this.changeUnit} setCity={this.setCity}/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import '../css/NavBar.css';
+import axios from 'axios';
+
+class NavBar extends Component {
+    state = {
+        data: []
+    }
+    handleChange = (e) => {
+        if (e.target.value !== '') {
+            axios.get(`http://autocomplete.travelpayouts.com/places2?term=${e.target.value}&locale=en&type=city`)
+                .then(res => {
+                    this.setState({
+                        data: res.data
+                    });
+                });
+        } else {
+            this.setState({
+                data: []
+            });
+        }
+    }
+
+
+    render() {
+        let { data } = this.state;
+        let autoCompleteList = null;
+        if (data.length !== 0) {
+            autoCompleteList =
+                <div className="sugesstions">
+                    {data.map(city => {
+                        if (city.type === 'city') {
+                            return (
+                                <div className="city-item" key={Math.random()} onClick={() => { this.props.setCity(city.name) }}>
+                                    <p>{city.name} {city.country_name}</p>
+                                </div>
+                            )
+                        } else {
+                            return null;
+                        }
+                    })}
+                </div>
+        }
+
+        return (
+            <div className="nav">
+                <h2 className="logo">Weather App</h2>
+                <input className="city-search" placeholder="city name..." onChange={this.handleChange} />
+                <div className="unit">
+                    <span onClick={() => { this.props.changeUnit('C') }}>C</span>
+                    <span>|</span>
+                    <span onClick={() => { this.props.changeUnit('F') }}>F</span>
+                </div>
+                {autoCompleteList}
+
+            </div>
+        )
+    }
+}
+
+export default NavBar;
