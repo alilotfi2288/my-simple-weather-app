7dd04b408fc4ab9b44ce5f0709ad159d216deb0e
alilotfi.dev <lotfi26.ali@gmail.com>
2020-08-16 13:00:03 +0430

    now let`s go for tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import WeatherBox from './components/WeatherBox';
+import Loader from './components/Loader';
 import { weatherConfig } from './api/weatherConfig';
 import axios from 'axios';
 
@@ -8,19 +9,28 @@ class App extends Component {
   state = {
     unit: 'C',
     city: '',
-    data: ''
+    data: '',
+    isLoading: false
   }
   changeUnit = (unit) => {
+    //changing the unit of weather and wind speed
     this.setState({
       unit
     })
   }
   getWeather = (city) => {
     if (city !== '') {
+      //show loading
+      this.setState({
+        isLoading: true
+      });
+      //weather config give us the url that we want
       axios.get(weatherConfig(city))
         .then(res => {
+          //set data and hide loading
           this.setState({
-            data: res.data
+            data: res.data,
+            isLoading: false
           });
         });
 
@@ -32,13 +42,17 @@ class App extends Component {
     });
     this.getWeather(city)
   }
- 
+
   render() {
+    //check if loading should display
+    const loading = this.state.isLoading ? (<Loader />) : (null);
+
     return (
       <div className="App">
         <NavBar changeUnit={this.changeUnit} setCity={this.setCity} />
         <WeatherBox data={this.state.data} unit={this.state.unit} />
-        <div className="copy-right">Powered by <a href="https://www.weatherapi.com/" title="Free Weather API">WeatherAPI.com</a></div>
+        {loading}
+        <div className="copy-right">Powered by <a href="https://www.weatherapi.com/" title="Free Weather API">WeatherAPI.com</a> and developed with &#10084;&#65039; by Ali Lotfi</div>
       </div>
     );
   }
---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/Loader.css';
+
+const Loader = () => {
+    return (
+        <div className="loader-container">
+            <div className="loading"></div>
+            <div className="loading"></div>
+            <div className="loading"></div>
+        </div>
+    )
+};
+
+export default Loader;
