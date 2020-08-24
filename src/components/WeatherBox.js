d79654c5263f6fc2e474aba4c683ec1424894838
alilotfi.dev <lotfi26.ali@gmail.com>
2020-08-24 14:34:57 +0430

    responsive

--- src/components/WeatherBox.js
+++ src/components/WeatherBox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import WindImage from '../img/wind.svg';
-import '../css/WeatherBox.min.css'
+import '../css/WeatherBox.css'
 
 class WeatherBox extends Component {
     render() {
